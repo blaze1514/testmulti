e553716e3a4123052fa130608f782f2651857cc4
blaze <eswarch696@gmail.com>
2022-03-31 21:41:30 +0530

    apps '

---
+++ one/src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+class App extends React.Component {
+  // componentDidMount() {
+  //   const script = document.createElement("script");
+
+  //   script.src = "";
+  //   script.async = true;
+
+  //   document.body.appendChild(script);
+  // }
+
+  render() {
+    return (
+      <div>
+        hey
+        {/* <div id="root">
+      {/* <script src='http://localhost:5000/static/js/main.3c60973b.js' ></script> */}
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ two/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <p>
+    Edit <code>src/App.js</code> and oyee.
+  </p>
+  );
+}
+
+export default App;
