ee828ca344391fd0311cc7aee9ad51dc45c00b06
blaze <eswarch696@gmail.com>
2022-04-20 15:27:35 +0530

    rough layout

--- two/src/App.js
+++ two/src/App.js
@@ -1,12 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BsChatRightDots, BsArrowsAngleContract } from "react-icons/bs";
+import { CgArrowsExpandLeft } from "react-icons/cg";
+class App extends React.Component {
+  state = {
+    open: true,
+    expand: false,
+  };
 
-function App() {
-  return (
-    <p>
-    Edit <code>src/App.js</code> and oyee.
-  </p>
-  );
+  render() {
+    const { open, expand } = this.state;
+    return (
+      <React.Fragment>
+        <div>
+          {!open ? (
+            <button
+              class="open-button"
+              onClick={() =>
+                this.setState({ open: true }, () => {
+                  console.log("clicked", this.state.open);
+                })
+              }
+            >
+              <BsChatRightDots size={22} color={"blue"} />
+            </button>
+          ) : (
+            <div className= {expand ? "center-box" :"chat-popup"}  id="myForm">
+              <div
+                style={{
+                  width: "100%",
+                  background: "blue",
+                  color: "white",
+                  padding: "10px",
+                }}
+              >
+                Header
+                <span style={{ float: "right" }}>
+                  {expand ? (
+                    <BsArrowsAngleContract
+                      onClick={() => this.setState({ expand: false })}
+                    />
+                  ) : (
+                    <CgArrowsExpandLeft
+                      onClick={() => this.setState({ expand: true })}
+                    />
+                  )}
+                  &nbsp;&nbsp;
+                  <span
+                    onClick={() => {
+                      this.setState({ open: false });
+                    }}
+                  >
+                    {" "}
+                    X
+                  </span>
+                </span>
+              </div>
+              <form class="form-container">
+                <label for="msg">
+                  <b>Message</b>
+                </label>
+                <textarea
+                  placeholder="Type message.."
+                  name="msg"
+                  required
+                ></textarea>
+
+                <button type="submit" class="btn">
+                  Send
+                </button>
+                <button
+                  type="button"
+                  class="btn cancel"
+                  onClick={() => this.setState({ open: false })}
+                >
+                  Close
+                </button>
+              </form>
+            </div>
+          )}
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
